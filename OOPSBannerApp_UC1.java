2ac2b14874fafd67d15666b4fdcf0f1a68a63294
jay5818 <68957356+Jay59376@users.noreply.github.com>
2026-02-17 04:26:15 +0530

    UC2: Render OOPS Banner (Aligned + Backup Saved)

---
+++ OOPSBannerApp_UC1.java
@@ -0,0 +1,26 @@
+
+public class OOPSBannerApp_UC1 {
+
+    public static void main(String[] args) {
+        // Row 1: Top of O, O, P, S
+        System.out.println("  ***** " + "   " + "  ***** " + "   " + " ****** " + "   " + "  ***** ");
+        
+        // Row 2: Upper arches
+        System.out.println(" * * " + "   " + " * * " + "   " + " * * " + "   " + " * * ");
+        
+        // Row 3: Mid-upper
+        System.out.println("* *" + "   " + "* *" + "   " + "* *" + "   " + "* ");
+        
+        // Row 4: Middle (P has a bar, S snakes back)
+        System.out.println("* *" + "   " + "* *" + "   " + " ****** " + "   " + "  ***** ");
+        
+        // Row 5: Mid-lower
+        System.out.println("* *" + "   " + "* *" + "   " + "* " + "   " + "       * ");
+        
+        // Row 6: Lower arches
+        System.out.println(" * * " + "   " + " * * " + "   " + "* " + "   " + " * * ");
+        
+        // Row 7: Bottom
+        System.out.println("  ***** " + "   " + "  ***** " + "   " + "* " + "   " + "  ***** ");
+    }
+}
