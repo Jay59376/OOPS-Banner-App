502ba22720ab7d18a30a5e23161d070d85961b6e
jay5818 <68957356+Jay59376@users.noreply.github.com>
2026-02-17 14:38:21 +0530

    UC6 Banner App using static helper functions

---
+++ UC6BannerApp.java
@@ -0,0 +1,57 @@
+class UC6BannerApp {
+
+    static String[] getOPattern() {
+        return new String[] {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    static String[] getPPattern() {
+        return new String[] {
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        String gap = "   ";
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(gap, o[i], o[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
